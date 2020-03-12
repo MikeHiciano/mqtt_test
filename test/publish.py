32695028966790c8cbaa8972b06df4508f9188ce
Miguel Hiciano <mike29hiciano@gmail.com>
2020-03-12 00:03:27 -0400

    publisher update

--- test/publish.py
+++ test/publish.py
@@ -2,4 +2,8 @@ import paho.mqtt.publish as publish
 
 broker_address = "10.0.0.13"
 topic = "sum_topic"
-publish.single(topic,"hello another", hostname=broker_address,client_id='someone')
+while True:
+    publish.single(topic,"on", hostname=broker_address,client_id='someone')
+    time.sleep(1)
+    publish.single(topic,"off", hostname=broker_address,client_id='someone')
+    time.sleep(1)

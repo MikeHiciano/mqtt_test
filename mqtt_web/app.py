72268825bbb7bc680af2f2dc1dda6b9c2bdab0eb
Miguel Hiciano <mike29hiciano@gmail.com>
2020-03-16 18:19:45 -0400

    adding web server

--- mqtt_web/app.py
+++ mqtt_web/app.py
@@ -1,8 +1,22 @@
-from flask import Flask, jsonify
-import paho.mqtt.client as mqtt
+from flask import Flask, jsonify, render_template, request
+import paho.mqtt.publish as publish 
 
 app = Flask(__name__)
 
-broker_address = "localhost"
-broker_port = 1883
-topic = 'sum_topic'
+broker_address = "10.0.0.13" # local broker ip
+broker_port = 1883 # mqtt local brocker port
+topic = 'sum_topic' # the topic
+
+@app.route('/' ,methods=['GET','POST'])
+def buttons():
+    if request.method == 'POST':
+        if request.form.get("on"):
+            publish.single(topic,"on", hostname=broker_address,client_id='someone')
+        
+        elif request.form.get("off"):
+            publish.single(topic,"off", hostname=broker_address,client_id='someone')
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
